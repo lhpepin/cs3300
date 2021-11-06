1ed67ff5aa3e27b539c251dfa6b57f3b82447613
Liam Pepin <lpepin@localhost.localdomain>
2021-11-06 12:03:44 -0600

    Finished implementing rspec testing

---
+++ spec/examples/first_spec.rb
@@ -0,0 +1,11 @@
+
+require "rails_helper"
+
+describe "First example" do 
+  describe "The math below is wrong..." do
+    it "should equal 42 but we said 43" do
+      expect(6 * 7).to eq(42)
+    end
+  end
+end
+
