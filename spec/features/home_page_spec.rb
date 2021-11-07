5c039d9ecfa174dfc71bec9fc5748940d9f56bca
Liam Pepin <lpepin@localhost.localdomain>
2021-11-07 10:44:03 -0700

    Implemented spec for real this time

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
 
-RSpec.feature "HomePages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+require "rails_helper"
+
+RSpec.feature "Visiting the homepage", type: :feature do
+  scenario "The visitor should see projects" do
+    visit root_path
+    expect(page).to have_text("Projects")
+  end
 end
+
+
