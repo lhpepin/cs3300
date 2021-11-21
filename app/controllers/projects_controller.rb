6ba40a9a173719f93d9a1679783b39708231ee58
Liam Pepin <lpepin@localhost.localdomain>
2021-11-21 13:48:23 -0700

    added factory bot

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
+  befoer_action :authenticate_user!
 
   # GET /projects or /projects.json
   def index
