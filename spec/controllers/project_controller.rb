5ae36dee7564d5b0d8ecfb44b74856243c38cabb
Liam Pepin <lpepin@localhost.localdomain>
2021-11-30 09:53:45 -0700

    changed some tests

--- spec/controllers/project_controller.rb
+++ spec/controllers/project_controller.rb
@@ -2,21 +2,20 @@ require "rails_helper"
 
 RSpec.describe ProjectsController, type: :controller do
 
-  
-
+  let(:user){ login_user }
   #testing the root page returns sucessfully
   context "GET #index" do
-    let(:user){ login_user }
     it "returns a success response" do
       get :index
       expect(response).to be_successful
     end
   end
 
+
   #testing to ensure project pages return sucessfully
   context "GET #show" do
-    let(:user){ login_user }
     let!(:project) { Project.create(title: "Test title", description: "Test description") }
+
     it "returns a success response" do
       get :show, params: { id: project }
       expect(response).to be_successful
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,15 @@ require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
-  add_filter '/spec/' # for rspec
+  add_filter '/spec/'
+
+  filters.clear# This will remove the :root_filter and :bundler_filter that come via simplecov's defaults
+
+  add_filter do |src|
+
+  !(src.filename =~ /^#{SimpleCov.root}/) unless src.filename =~ /my_engine/
+
+ end 
 end
 
 #devise and login stuff
